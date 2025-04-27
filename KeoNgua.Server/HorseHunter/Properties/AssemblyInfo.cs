46f371004164422dcbba6feeb1c47b8ea89700d9
nguyensitien <nguyensitien481994@gmail.com>
2025-04-27 22:26:28 +0700

    no message

--- KeoNgua.Server/HorseHunter/Properties/AssemblyInfo.cs
+++ KeoNgua.Server/HorseHunter/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("21894e6e-d7f5-493f-8abd-70ed7907d1ce")]
+[assembly: Guid("18f23dfa-c757-495b-bf2d-35e27ac28f76")]
 
 // Version information for an assembly consists of the following four values:
 //
